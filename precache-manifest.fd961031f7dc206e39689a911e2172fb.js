065dcec5784596b543f6c56e057a6f923042ad25
Barth37 <brian.arth@gmail.com>
2019-06-10 19:33:47 -0400

    Updates

---
+++ precache-manifest.fd961031f7dc206e39689a911e2172fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6cf9c5e3f735914e3ff28e49dfbae41",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "71bc980d3f9075b0def5",
+    "url": "/clickygame/static/css/main.2c7b4234.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "71bc980d3f9075b0def5",
+    "url": "/clickygame/static/js/main.9a5d3728.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
